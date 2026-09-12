a839f6d654e4f83651283f01316379ed9dcee33f
Joowon Jung <neont21@gmail.com>
2026-09-12 17:45:32 +0900

    feat: implement view layout in lib/pages/term_of_service_page.dart & lib/pages/privacy_page.dart #7

--- lib/pages/privacy_page.dart
+++ lib/pages/privacy_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PrivacyPage extends StatelessWidget {
   const PrivacyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Center(child: Text('Privacy')));
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              context.pop();
+            },
+            icon: Icon(Icons.chevron_left),
+          ),
+          title: Text('개인정보 처리방침'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+            ),
+        )
+    );
   }
 }
--- lib/pages/term_of_service_page.dart
+++ lib/pages/term_of_service_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class TermOfServicePage extends StatelessWidget {
   const TermOfServicePage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Center(child: Text('ToS')));
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              context.pop();
+            },
+            icon: Icon(Icons.chevron_left),
+          ),
+          title: Text('이용약관'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+          ),
+        )
+    );
   }
 }
