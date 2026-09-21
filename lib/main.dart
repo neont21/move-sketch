32f7558f73f1bff142e5c1388b59b3ea0bf13c93
Joowon Jung <neont21@gmail.com>
2026-09-21 23:18:33 +0900

    chore: configure Firebase and initialize app in main #29

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'firebase_options.dart';
 import 'routing/app_router.dart';
 import 'ui/core/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await initializeDateFormatting('ko', null);
   runApp(const MoveSketchApp());
 }
@@ -21,4 +26,3 @@ class MoveSketchApp extends StatelessWidget {
     );
   }
 }
-
